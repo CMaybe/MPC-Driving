a1f745bade9bc1430a1e255094e272bce3eb1211
CMaybe <asbsds123@gmail.com>
2024-12-04 14:25:32 +0000

    chore: change wheel_base as const value

--- MPCDriving/include/mpc/model/SystemModel.hpp
+++ MPCDriving/include/mpc/model/SystemModel.hpp
@@ -9,28 +9,30 @@
 class SystemModel {
 public:
     SystemModel() = default;
-    SystemModel(double dt, double wheel_base);
+    SystemModel(double dt);
     SystemModel(const SystemModel& other);
 
     void setState(double x, double y, double yaw, double velocity);
     void setState(const State& other);
     void setState(const Eigen::Vector4d& state_vector);
     void updateModel(double yaw, double velocity, double steer);
+    Eigen::Vector4d updateState(const Eigen::Vector4d& state, const Eigen::Vector2d& input);
 
     Eigen::Matrix4d getA() const;
     Eigen::Matrix4d getB() const;
     Eigen::Matrix4d getC() const;
     Eigen::Matrix4d getAd() const;
     Eigen::Matrix4d getBd() const;
+    State getState() const;
 
+    double getDt() const;
     double getWheelBase() const;
 
 private:
     double dt_;
-    double wheel_base_;
+    const double wheel_base_ = 3.5;
 
     void updateDiscretizedModel();
-    Eigen::Vector4d updateState(const Eigen::Vector4d& state, const Eigen::Vector4d& input);
     Eigen::Matrix4d A_, B_, C_;
     Eigen::Matrix4d Ad_, Bd_;
 
--- MPCDriving/src/model/SystemModel.cpp
+++ MPCDriving/src/model/SystemModel.cpp
@@ -1,15 +1,10 @@
 #include "mpc/model/SystemModel.hpp"
+#include "SystemModel.hpp"
 
-SystemModel::SystemModel(double dt, double wheel_base) : dt_(dt), wheel_base_(wheel_base) {}
+SystemModel::SystemModel(double dt) : dt_(dt) {}
 SystemModel::SystemModel(const SystemModel& other)
-    : dt_(other.dt_)
-    , wheel_base_(other.wheel_base_)
-    , A_(other.A_)
-    , B_(other.B_)
-    , C_(other.C_)
-    , Ad_(other.Ad_)
-    , Bd_(other.Bd_)
-    , state_(other.state_) {}
+    : dt_(other.dt_), Bd_(other.Bd_), state_(other.state_) {}
+
 void SystemModel::setState(double x, double y, double yaw, double velocity) {
     state_ = State(x, y, yaw, velocity);
 }
@@ -17,7 +12,6 @@ void SystemModel::setState(const State& other) { state_ = State(other); }
 void SystemModel::setState(const Eigen::Vector4d& state_vector) { state_ = State(state_vector); }
 
 void SystemModel::updateModel(double yaw, double velocity, double steer) {
-    A_ = Eigen::MatrixXd(4, 4);
     A_.row(0) << 0.0, 0.0, -velocity * std::sin(yaw), std::cos(yaw);
     A_.row(1) << 0.0, 0.0, velocity * std::cos(yaw), std::sin(yaw);
     A_.row(2) << 0.0, 0.0, 0.0, std::tan(steer) / wheel_base_;
@@ -42,8 +36,14 @@ void SystemModel::updateDiscretizedModel() {
 }
 
 Eigen::Vector4d SystemModel::updateState(const Eigen::Vector4d& state,
-                                         const Eigen::Vector4d& input) {
-    return Ad_ * state + Bd_ * input;
+                                         const Eigen::Vector2d& input) {
+    // return Ad_ * state + Bd_ * input;
+    state_.updateState(input[0], input[1], dt_, wheel_base_);
+    return state_.getState();
+    // (x0 + vel0 * CppAD::cos(yaw0) * dt_);
+    // fg[1 + y_idx_ + t] = y1 - (y0 + vel0 * CppAD::sin(yaw0) * dt_);
+    // fg[1 + yaw_idx_ + t] = yaw1 - (yaw0 + vel0 / wheel_base_ * CppAD::tan(steer0) * dt_);
+    // fg[1 + velocity_idx_ + t] = vel1 - (vel0 + acc0 * dt_);
 }
 
 Eigen::Matrix4d SystemModel::getA() const { return A_; }
@@ -51,4 +51,7 @@ Eigen::Matrix4d SystemModel::getB() const { return B_; }
 Eigen::Matrix4d SystemModel::getC() const { return C_; }
 Eigen::Matrix4d SystemModel::getAd() const { return Ad_; }
 Eigen::Matrix4d SystemModel::getBd() const { return Bd_; }
+
+State SystemModel::getState() const { return state_; }
 double SystemModel::getWheelBase() const { return wheel_base_; }
+double SystemModel::getDt() const { return dt_; }
